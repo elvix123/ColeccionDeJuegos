445af5d9470898fd09a159bfbe8ee927c296eef1
Elvis Quecara Cruz <elvisqc20@gmail.com>
2023-10-28 12:03:33 -0500

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Elvis Quecara Cruz on 28/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
